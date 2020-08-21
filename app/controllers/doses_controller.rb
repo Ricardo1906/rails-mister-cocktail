825ddc3adc2de888c43415244b0bb211a44d184f
Ricardo Rodrigues <ricardo.rodriguesevo@gmail.com>
2020-08-21 16:02:54 +0100

    heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,31 +1,26 @@
 class DosesController < ApplicationController
-before_action :set_cocktail, only: :create
 
-  def create
+def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render 'cocktails/show'
-      end
+    
+      render "cocktails/show"
+    end
   end
-  
+
   def destroy
     @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
-  
-    private
-  
-  def set_cocktail
-   @cocktail = Cocktail.find(params[:cocktail_id])
-  end
-  
+
+  private
+
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
-    end
   end
 end
